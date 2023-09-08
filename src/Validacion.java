8233451fa81e6fa8f3e2c1643785305edb35a857
vina <vina@VIAWSE02LC0428.duoc.acad>
2023-09-07 22:04:16 -0300

    Prueba

---
+++ src/Validacion.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author vina
+ */
+public class Validacion {
+    public  Vehiculo autito;
+    public  Cliente clientito;
+    public void validar()
+    {
+        autito.getKilometraje();
+        autito.getPatente();
+        autito.getAño();
+        clientito.getTelefono();//intetente lo que pude profe, hizo falta mas practica :c
+    }
+    
+}
